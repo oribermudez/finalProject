b27ac0c70d48ffe0e5e266c447e7ef1f126aaadb
Ori Bermudez <ori.bermudez@outlook.com>
2018-03-21 17:53:19 -0600

    Deploying in Heroku

--- server/app.js
+++ server/app.js
@@ -52,6 +52,10 @@ app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/tickets', tickets);
 
+app.all('/*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
